45af8286e9e84f52fad3f34d6e16e0daa60d7d12
Vivek Sondhiya <98210740+viveksondhiya@users.noreply.github.com>
2023-02-14 23:48:56 +0530

    Update StudentDaoImpl.java

--- spring_jdbc/springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ spring_jdbc/springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -3,14 +3,13 @@ package com.spring.jdbc.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-
 import com.spring.jdbc.entities.Student;
 
+@Component("studentDao")
 public class StudentDaoImpl implements StudentDao {
-
+        @Autowired
 	private JdbcTemplate jdbcTemplate;
 	
 	public int insert(Student student) {
@@ -65,7 +64,7 @@ public class StudentDaoImpl implements StudentDao {
 	public JdbcTemplate getJdbcTemplate() {
 		return jdbcTemplate;
 	}
-
+        @Autowired
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
